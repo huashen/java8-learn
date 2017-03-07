af5e14671acefa4ff658d102a126b2d330c1e9b3
huashen <huashen361@gmail.com>
2017-03-07 22:03:34 +0800

    git add2

--- src/main/java/com/lhs/learn/strean/TraderAndTransactionTest.java
+++ src/main/java/com/lhs/learn/strean/TraderAndTransactionTest.java
@@ -24,8 +24,24 @@ public class TraderAndTransactionTest {
                 new Transaction(mario, 2012, 700),
                 new Transaction(alan, 2012, 950));
 
-        //找出2011年的所有交易并按交易额排序
+        //1.找出2011年的所有交易并按交易额排序
         List<Transaction> collect = transactions.stream().filter(d -> d.getYear() == 2011).sorted(Comparator.comparing(Transaction::getValue)).collect(Collectors.toList());
 System.out.println(collect);
+
+        //2.交易员都在哪些不同的城市待过
+        List<String> cities = transactions.stream().map(d -> d.getTrader().getCity()).distinct().collect(Collectors.toList());
+System.out.println(cities);
+
+        //3.查找出所有来自剑桥的交易员，并按姓名排序
+        List<Trader> traderList = transactions.stream().map(d -> d.getTrader()).distinct().filter(d -> d.getCity() == "Cambridge").sorted(Comparator.comparing(Trader::getName)).collect(Collectors.toList());
+System.out.println(traderList);
+
+        //4.返回所有交易员的姓名字符串，按字母排序
+        String nameReduce = transactions.stream().map(d -> d.getTrader().getName()).distinct().sorted().reduce("", (n1, n2) -> n1 + n2);
+System.out.println(nameReduce);
+
+        //5.有没有交易员是在米兰工作的
+        boolean milan = transactions.stream().anyMatch(d -> d.getTrader().getCity().equals("Milan"));
+System.out.println(milan);
     }
 }
